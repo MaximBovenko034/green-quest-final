2e749b9405384151dfd7fba4951f6846148fa8e6
MaximBovenko034 <maxim.bovenko@e-u.edu.ua>
2025-08-26 16:04:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
+document.addEventListener("mousemove", (e) => {
+  const x = (window.innerWidth - e.pageX) / 100;
+  const y = (window.innerHeight - e.pageY) / 100;
 
-document.addEventListener('mousemove', e => {
-  const layers = document.querySelectorAll('.layer');
-  layers.forEach((layer, i) => {
-    const speed = (i + 1) * 10;
-    const x = (window.innerWidth - e.pageX*speed/100)/100;
-    const y = (window.innerHeight - e.pageY*speed/100)/100;
-    layer.style.transform = `translateX(${x}px) translateY(${y}px) scale(${1+(i*0.2)})`;
-  });
+  document.querySelector(".back").style.transform = `translate(${x}px, ${y}px) scale(2)`;
+  document.querySelector(".middle").style.transform = `translate(${x/2}px, ${y/2}px)`;
+  document.querySelector(".front").style.transform = `translate(${x/4}px, ${y/4}px) scale(0.8)`;
 });
